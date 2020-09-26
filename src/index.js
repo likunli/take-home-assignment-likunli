134c2764c6389dd7372a2fb91b1026888fa5531a
Likun Li <likunli@Likuns-MacBook-Pro.local>
2020-09-26 00:14:48 -0700

    add basename for github page

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <App />
       </Router>
     </Provider>
