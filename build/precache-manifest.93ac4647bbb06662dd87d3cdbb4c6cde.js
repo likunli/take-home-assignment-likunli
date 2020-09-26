73487e35d6aaae5f6c204c1053134f75af9cac26
Likun Li <likunli@Likuns-MacBook-Pro.local>
2020-09-25 23:41:39 -0700

    add github page deployment config

---
+++ build/precache-manifest.93ac4647bbb06662dd87d3cdbb4c6cde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97741cd71f6dbd62ea5db3ead646a59",
+    "url": "//likunli.github.io/take-home-assignment-likunli/index.html"
+  },
+  {
+    "revision": "86fcf421baf14327e80a",
+    "url": "//likunli.github.io/take-home-assignment-likunli/static/css/main.6bfcedac.chunk.css"
+  },
+  {
+    "revision": "7f0169ba08fe105cb66e",
+    "url": "//likunli.github.io/take-home-assignment-likunli/static/js/2.f5a4b36e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "//likunli.github.io/take-home-assignment-likunli/static/js/2.f5a4b36e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86fcf421baf14327e80a",
+    "url": "//likunli.github.io/take-home-assignment-likunli/static/js/main.848e0e8c.chunk.js"
+  },
+  {
+    "revision": "73eac2d2ea846238cd09",
+    "url": "//likunli.github.io/take-home-assignment-likunli/static/js/runtime-main.e00699b8.js"
+  }
+]);
